e45b3d7931c2d195bd2b4313814b9b085c285ebb
jus37 <jus_37@outlook.jp>
2020-10-20 21:37:51 +0900

    comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name]) #deviseにnameが保存できるようにストロングパラメーターを設定
   end
 end

e1aa4e34104ea81dfb54ec9e48fda4242bbb2805
jus37 <jus_37@outlook.jp>
2020-10-21 17:16:13 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   # 本来新規投稿ページはnewだが一覧ページに投稿フォームがあるのでindexがインスタンス変数を生成している
   def create
@@ -10,7 +11,9 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      render: :index
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
   end
 
   private
